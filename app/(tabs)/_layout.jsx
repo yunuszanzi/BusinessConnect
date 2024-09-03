e258ffacdfd8aa1c257fb4152ded289cbf323c83
Yunus-Mustafa <yunus.mustafa@sosglobal.pk>
2024-09-03 17:06:22 +0500

    Login Logout Initials code

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,12 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { Colors } from "./../../constants/Colors";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: Colors.primary,
       }}
     >
       <Tabs.Screen
---
+++ app/_layout.jsx
@@ -0,0 +1,30 @@
+import { ClerkProvider, SignedIn } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import LoginScreen from "./../components/LoginScreen";
+
+export default function RootLayout() {
+  useFonts({
+    "outfit-regular": require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SignedIn>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
+    </ClerkProvider>
+  );
+}
